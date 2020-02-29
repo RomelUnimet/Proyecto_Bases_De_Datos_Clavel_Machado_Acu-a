97d7de099e9725a8f68ecdc56fd55f9ef43c229f
WilCloud <wilfredo.machado@correo.unimet.edu.ve>
2020-02-29 10:20:38 -0400

    commit_1

--- frontend/gui/src/components/productForm.js
+++ frontend/gui/src/components/productForm.js
@@ -1,28 +1,24 @@
-import React from 'react';
-import { Form, Input, Button, Select, InputNumber } from 'antd';
-import axios from 'axios';
+import React from "react";
+import { Form, Input, Button, Select, InputNumber } from "antd";
+import axios from "axios";
 
 const { Option } = Select;
+
 const layout = {
   labelCol: {
-    span: 11,
+    span: 11
   }
 };
+
 const tailLayout = {
   wrapperCol: {
     offset: 8,
-    span: 16,
-  },
+    span: 16
+  }
 };
 
 class PForm extends React.Component {
   formRef = React.createRef();
-
-  onGenderChange = value => {
-    this.formRef.current.setFieldsValue({
-      note: `Hi, ${value === 'male' ? 'man' : 'lady'}!`,
-    });
-  };
 
   onFinish = values => {
     console.log(values);
@@ -32,123 +28,162 @@ class PForm extends React.Component {
     this.formRef.current.resetFields();
   };
 
-  onFill = () => {
-    this.formRef.current.setFieldsValue({
-      note: 'Hello world!',
-      gender: 'male',
-    });
+  state = {
+    providers: [],
+    category: []
   };
 
-    state = {
-        providers: []
+  componentDidMount() {
+    axios.get("http://127.0.0.1:8000/rest/prov/").then(res => {
+      this.setState({
+        providers: res.data
+      });
+      console.log(this.state.providers);
+    });
+
+    axios.get("http://127.0.0.1:8000/rest/category/").then(ras => {
+      this.setState({
+        category: ras.data
+      });
+      console.log(this.state.category);
+    });
+  }
+
+  handleFormSubmit = (event, requestType, productID) => {
+    //event.preventDefault();
+    const name = event.target.elements.name.value;
+    const pasillo = event.target.elements.pasillo.value;
+    const category = event.target.elements.category.value;
+    const provider = event.target.elements.provider.value;
+
+    switch (requestType) {
+      case "post":
+        return axios
+          .post("http://127.0.0.1:8000/rest/prod/", {
+            product_name: name,
+            hall: pasillo,
+            category: category,
+            provider: provider
+          })
+          .then(res => console.log(res))
+          .catch(error => console.err(error));
+
+      case "put":
+        return axios
+          .put(`http://127.0.0.1:8000/rest/prod/${productID}/`, {
+            product_name: name,
+            hall: pasillo
+          })
+          .then(res => console.log(res))
+          .catch(error => console.err(error));
     }
-
-    componentDidMount(){
-        axios.get('http://127.0.0.1:8000/rest/prov/').then(res => {
-            this.setState({
-                providers: res.data
-            });
-        })
-    }
-
-    bringProviders = () =>{
-
-    }
-
-    handleFormSubmit = (event, requestType, productID) => {
-        //event.preventDefault();
-        const name = event.target.elements.title.value;
-        const pasillo = event.target.elements.pasillo.value;
-        
-        switch(requestType){
-            case 'post':
-                return axios.post('http://127.0.0.1:8000/rest/prod/', {
-                    product_name:name,
-                    hall: pasillo
-                }).then(res => console.log(res)).catch(error => console.err(error));
-            
-            case 'put':
-                return axios.put(`http://127.0.0.1:8000/rest/prod/${productID}/`, {
-                    product_name:name,
-                    hall: pasillo
-                }).then(res => console.log(res)).catch(error => console.err(error));
-        }
-
-    }
-
+  };
 
   render() {
     return (
-      <Form {...layout} ref={this.formRef} name="control-ref" onFinish={this.onFinish} onSubmit={(event) => this.handleFormSubmit(
-          event, this.props.requestType, this.props.productID
-      )}>
+      <Form
+        {...layout}
+        ref={this.formRef}
+        name="control-ref"
+        onFinish={this.onFinish}
+        onSubmit={event =>
+          this.handleFormSubmit(
+            event,
+            this.props.requestType,
+            this.props.productID
+          )
+        }
+      >
         <Form.Item
           name="Nombre"
-          
+          rules={[
+            {
+              required: true
+            }
+          ]}
           label="Nombre"
         >
           <Input name="name" placeholder="Nombre del producto" />
         </Form.Item>
 
-        <Form.Item 
-            name="Pasillo" 
-            label="Pasillo" 
-            rules={[
-                { 
-                type: 'number', min: 0, max: 99 
-                }
-            ]}
+        <Form.Item
+          name="Pasillo"
+          label="Pasillo"
+          rules={[
+            {
+              type: "number",
+              min: 0,
+              max: 99,
+              required: true
+            }
+          ]}
         >
-        <InputNumber name="pasillo" placeholder="Nro del pasillo"/>
+          <InputNumber name="pasillo" placeholder="Nro del pasillo" />
         </Form.Item>
 
-        {/* <Form.Item
+        <Form.Item
           name="Proveedor"
           label="Proveedor"
           rules={[
             {
-              required: true,
-            },
+              required: true
+            }
           ]}
         >
-          <Select
-            placeholder="Selecciona un proveedor"
-            onChange={this.onGenderChange}
-            allowClear
-          >
-            
-            
-            <Option value="male">male</Option>
-            <Option value="female">female</Option>
-            <Option value="other">other</Option>
+          <Select placeholder="Selecciona un proveedor" allowClear>
+            {this.state.providers.map(provs => (
+              <Option value={provs.name} key={provs.name}>
+                {provs.name}
+              </Option>
+            ))}
           </Select>
         </Form.Item>
+
+        <Form.Item
+          name="Categoria"
+          label="Categoria"
+          rules={[
+            {
+              required: true
+            }
+          ]}
+        >
+          <Select placeholder="Selecciona una categoria" allowClear>
+            {this.state.category.map(cats => (
+              <Option value={cats.name} key={cats.name}>
+                {cats.name}
+              </Option>
+            ))}
+          </Select>
+        </Form.Item>
+
         <Form.Item
           noStyle
-          shouldUpdate={(prevValues, currentValues) => prevValues.gender !== currentValues.gender}
+          shouldUpdate={(prevValues, currentValues) =>
+            prevValues.gender !== currentValues.gender
+          }
         >
           {({ getFieldValue }) =>
-            getFieldValue('gender') === 'other' ? (
+            getFieldValue("gender") === "other" ? (
               <Form.Item
                 name="customizeGender"
                 label="Customize Gender"
                 rules={[
                   {
-                    required: true,
-                  },
+                    required: true
+                  }
                 ]}
               >
                 <Input />
               </Form.Item>
             ) : null
           }
-        </Form.Item> */}
-        <Form.Item>
-            <Button type="primary" htmlType="submit">
-                {this.props.buttonText}
-            </Button>
         </Form.Item>
-
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            {this.props.buttonText}
+          </Button>
+        </Form.Item>
       </Form>
     );
   }
--- frontend/gui/src/containers/ProductListView.js
+++ frontend/gui/src/containers/ProductListView.js
@@ -1,36 +1,31 @@
-import React from 'react';
-import axios from 'axios';
-import Productss from '../components/products';
-import PForm from '../components/productForm';
+import React from "react";
+import axios from "axios";
+import Productss from "../components/products";
+import PForm from "../components/productForm";
 
-class ProductList extends React.Component{
+class ProductList extends React.Component {
+  state = {
+    products: []
+  };
 
-    state = {
-        products: []
-    }
+  componentDidMount() {
+    axios.get("http://127.0.0.1:8000/rest/prod/").then(res => {
+      this.setState({
+        products: res.data
+      });
+    });
+  }
 
-    componentDidMount(){
-        axios.get('http://127.0.0.1:8000/rest/prod/').then(res => {
-            this.setState({
-                products: res.data
-            });
-        })
-    }
-
-
-
-    render(){
-        return (
-            <>
-                <Productss data ={this.state.products}/>
-                <br/>
-                <h2>
-                    Crear un producto
-                </h2>
-                <PForm requestType="post" productID={null} buttonText="Crear"/>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <Productss data={this.state.products} />
+        <br />
+        <h2>Crear un producto</h2>
+        <PForm requestType="post" productID={null} buttonText="Crear" />
+      </>
+    );
+  }
 }
 
 export default ProductList;
